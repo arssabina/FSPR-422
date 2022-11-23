bd8395bcb5d8a51f78ce2dbfc5c7eaa9579d2911
arssabina <sabina121215@gmail.com>
2022-11-23 18:06:18 +0500

    modified lesson 22

--- semester_1/homework/lesson_22/lesson_22_д.з.py
+++ semester_1/homework/lesson_22/lesson_22_д.з.py
@@ -15,3 +15,14 @@
 # palindrom(33)
 
 # ============================================================
+# https://www.codewars.com/kata/5174a4c0f2769dd8b1000003/solutions/python
+# Sort numbers: Finish the solution so that it sorts the passed in array of numbers. 
+# If the function passes in an empty array or null/nil value then it should return an empty array.
+
+def solution(nums):
+    if nums:
+        print(sorted(nums))
+    else:
+        print([])
+
+solution([1,2,5,8,99])
