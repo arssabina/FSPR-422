d70460af300a9bdb73ecb02ca3f9567ac0c85e39
arssabina <sabina121215@gmail.com>
2022-11-23 09:22:30 +0500

    upd: palindrome number

---
+++ semester_1/CODEWARS_SOLUTIONS/palindrom.py
@@ -0,0 +1,36 @@
+# def palindrom(n):
+#     number=str(n)
+#     test=number[::-1]
+#     if number==test:
+#         print("It's palindrom number")
+#     else:
+#         print("It's not palindrome number")
+
+# palindrom(12)
+
+# =====================================================================
+# How to find next palindrome number   Мой вариант решения
+# def palindrom(n):
+#     n=n+1
+#     while True:
+#         n=str(n)        # переводим номер в строку 
+#         number=n[::-1]  # переворачиваем номер 
+#         if n==number:       
+#             print(int(n))
+#             break
+#         else: 
+#             n=int(n)
+#             n=n+1
+
+# palindrom(33)
+
+# ============================================================
+# Better solution:
+
+# def next_pal(val):
+#     val += 1
+#     while str(val) != str(val)[::-1]:
+#         val += 1
+#     print(val)
+
+# next_pal(11)
--- semester_1/КОНСПЕКТЫ УРОКА/les_22.class.py
+++ semester_1/КОНСПЕКТЫ УРОКА/les_22.class.py
@@ -1,11 +1,19 @@
 class Marker:
     size=15 # статичная переменная,  которая определяется до вызова
+    health=10
     # функции - методы
     # переменные - аттрибуты
     def __init__(self,company,color, price):
         self.company = company
         self.color = color   
         self.price=price
+
+        # метод класса
+    def draw(self, line_length):
+        if self.health <=0:
+            return "Маркер истощён" 
+        self.health-=line_length
+
 
 # динамичные аттрибуты, которые опред-ся после вызова функции 
 marker=Marker("Marker Inc.", "blue", 120)
@@ -18,3 +26,11 @@ print(marker_3.company, marker_3.color, marker_3.price)
 
 # не пишем print(self.company) потому что, self работает только внутри функции
 
+print("Health=", marker.health)
+marker.draw(5)
+print("Health=", marker.health)
+marker.draw(5)
+print("Health=", marker.health)
+marker.draw(5)
+print(marker.draw(11))
+print("Health=", marker.health)
