e40c62f48738960877c09da4aa6347bbc586e5ee
arssabina <sabina121215@gmail.com>
2022-12-01 09:22:02 +0500

    add: homework of lesson 25

--- semester_1/КОНСПЕКТЫ УРОКА/les_25.overloading_merhod.py
+++ semester_1/КОНСПЕКТЫ УРОКА/les_25.overloading_merhod.py
@@ -11,11 +11,14 @@ class Person:
     def __repr__(self):
         return f"Class info: name={self.name} age={self.age}"
 
+
+   
+
 person = Person("Sabina", 36)
 person_2= Person("Bohir", 36)
 print(person, person_2)       # если мы используем метод __str__, мы можем print писать так, чем печатать : print(person.name, person.age)
                             # мы также можем использовать метод __repr__ 
-                            
+                       
 # import datetime
 # date = datetime.datetime.now()
 # print(date, repr(date))
